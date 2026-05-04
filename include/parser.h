339eaa2c8f9940742f09e59575848081d6c79924
Mariam Mohamed <mariammxhamed@gmail.com>
2026-05-04 19:33:35 +0300

    improved parser

--- include/parser.h
+++ include/parser.h
@@ -1,8 +1,10 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <stdint.h>
+
 void extractOperands(const char *instruction, int *R1, int *R2, int imm);
-void parseInstruction(uint16_t instruction);
+void parseInstruction(const char *instruction);
 void loadProgram(const char *filename);
 
 #endif 
